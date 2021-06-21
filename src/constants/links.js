2b49eb9f04164475b38013314ca82966270550c9
ysknsid25 <ng_knok@docomo.ne.jp>
2021-06-22 07:12:43 +0900

    analutics 対応
    Signed-off-by: ysknsid25 <ng_knok@docomo.ne.jp>

--- src/constants/links.js
+++ src/constants/links.js
@@ -1,9 +1,11 @@
 export const noteUrl = "https://note.com/oshiani/";
 export const googleFormUrl = "https://forms.gle/PD5UiCAkRi41UTbL7";
 const twitterUtl = "https://twitter.com/intent/tweet?";
-const url = encodeURIComponent(location.href);
+//const url = encodeURIComponent(location.href);
+const campainUrl =
+    "https://oshiani.net/?utm_source=twitter&utm_medium=usershare&utm_campaign=twitterusershare";
 export const getTweetUrl = (title, hashTag, officialSite) => {
-    const tweet = "私の'推しアニ！'は『" + title + "』です！ " + url;
+    const tweet = "私の'推しアニ！'は『" + title + "』です！ " + campainUrl;
     const hashTags = hashTag + ",oshiani,推しアニ";
     return (
         twitterUtl +
@@ -18,8 +20,8 @@ export const getTweetUrl = (title, hashTag, officialSite) => {
 
 export const getOshiAniRecomendTweetUrl = () => {
     const hashTags = "oshiani";
-    const tweet = "あなたの推したいアニメを探すためのアプリ「推しアニ」です。";
-    return (
-        twitterUtl + "text=" + tweet + "&hashtags=" + hashTags + "&url=" + url
-    );
+    const tweet =
+        "あなたの推したいアニメを探すためのアプリ「推しアニ」です。" +
+        campainUrl;
+    return twitterUtl + "text=" + tweet + "&hashtags=" + hashTags;
 };
