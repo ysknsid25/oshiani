2baf2d895ca6002cda07499b38c50535bfc435f8
ysknsid25 <ng_knok@docomo.ne.jp>
2022-01-01 14:12:36 +0900

    [update] 配信頻度の項目を追加
    Signed-off-by: ysknsid25 <ng_knok@docomo.ne.jp>

--- src/components/profile/NotifySetting.vue
+++ src/components/profile/NotifySetting.vue
@@ -18,10 +18,41 @@
           align-self="center"
           justify="center"
         >
-          <v-switch v-model="isNotify" color="secondary"></v-switch>
+          A&G番組情報を配信希望
         </v-col>
         <v-col xs="4" md="4" align-self="center" justify="center">
-          通知を許可: {{ isNotify ? "する" : "しない" }}
+          <v-switch
+            v-model="isNotify"
+            color="secondary"
+            class="ml-8"
+          ></v-switch>
+        </v-col>
+      </v-row>
+      <v-row justify="space-around" no-gutters v-if="isNotify">
+        <!-- 頻度 一日一回 月一回  -->
+        <v-col
+          xs="5"
+          md="5"
+          offset-xs="3"
+          offset-md="3"
+          align-self="center"
+          justify="center"
+        >
+          配信頻度
+        </v-col>
+        <v-col xs="4" md="4" align-self="center" justify="center">
+          <v-radio-group v-model="howmanynotify" row class="ml-8">
+            <v-radio
+              label="毎日"
+              value="everyday"
+              checked="isEverydayChecked"
+            ></v-radio>
+            <v-radio
+              label="毎週月曜"
+              value="everymonday"
+              checked="isEveryMondayChecked"
+            ></v-radio>
+          </v-radio-group>
         </v-col>
       </v-row>
     </div>
@@ -34,6 +65,15 @@ export default {
   props: ["loading", "pgCastList", "pgTitleList"],
   data: () => ({
     isNotify: false,
+    howmanynotify: "everyday",
   }),
+  computed: {
+    isEverydayChecked: function () {
+      return this.howmanynotify === "everyday";
+    },
+    isEveryMondayChecked: function () {
+      return this.howmanynotify === "everymonday";
+    },
+  },
 };
 </script>
