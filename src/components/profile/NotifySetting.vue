6fb8e528140c8d308ce5fef553897a80fea524fa
ysknsid25 <ng_knok@docomo.ne.jp>
2022-01-02 10:31:43 +0900

    [update]メールアドレスの登録
    Signed-off-by: ysknsid25 <ng_knok@docomo.ne.jp>

--- src/components/profile/NotifySetting.vue
+++ src/components/profile/NotifySetting.vue
@@ -11,8 +11,8 @@
     <div v-if="!loading">
       <v-row justify="space-around" no-gutters>
         <v-col
-          xs="5"
-          md="5"
+          xs="3"
+          md="3"
           offset-xs="3"
           offset-md="3"
           align-self="center"
@@ -20,7 +20,7 @@
         >
           A&G番組情報を配信希望
         </v-col>
-        <v-col xs="4" md="4" align-self="center" justify="center">
+        <v-col xs="6" md="6" align-self="center" justify="center">
           <v-switch
             v-model="isNotify"
             color="secondary"
@@ -29,10 +29,9 @@
         </v-col>
       </v-row>
       <v-row justify="space-around" no-gutters v-if="isNotify">
-        <!-- 頻度 一日一回 月一回  -->
         <v-col
-          xs="5"
-          md="5"
+          xs="3"
+          md="3"
           offset-xs="3"
           offset-md="3"
           align-self="center"
@@ -40,7 +39,7 @@
         >
           配信頻度
         </v-col>
-        <v-col xs="4" md="4" align-self="center" justify="center">
+        <v-col xs="6" md="6" align-self="center" justify="center">
           <v-radio-group v-model="howmanynotify" row class="ml-8">
             <v-radio
               label="毎日"
@@ -55,17 +54,54 @@
           </v-radio-group>
         </v-col>
       </v-row>
+      <v-row justify="space-around" no-gutters v-if="isNotify">
+        <!-- 頻度 一日一回 月一回  -->
+        <v-col
+          xs="3"
+          md="3"
+          offset-xs="3"
+          offset-md="3"
+          align-self="center"
+          justify="center"
+        >
+          配信先
+        </v-col>
+        <v-col xs="4" md="4" align-self="center" justify="center">
+          <v-text-field
+            outlined
+            dense
+            v-model="email"
+            :rules="[rules.required, rules.email, rules.counter]"
+            label="E-mail"
+            class="ml-8 pt-6"
+          ></v-text-field>
+        </v-col>
+        <v-col xs="2" md="2" align-self="center" justify="center"> </v-col>
+      </v-row>
     </div>
   </v-container>
 </template>
 
 <script>
+const pattern =
+  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const maxSize = 50;
 export default {
   name: "NotifySetting",
   props: ["loading", "pgCastList", "pgTitleList"],
   data: () => ({
     isNotify: false,
     howmanynotify: "everyday",
+    email: "",
+    isEmailValid: false,
+    rules: {
+      required: (value) => !!value || "Required.",
+      counter: (value) =>
+        value.length <= maxSize || "Max " + maxSize + " characters",
+      email: (value) => {
+        return pattern.test(value) || "Invalid e-mail.";
+      },
+    },
   }),
   computed: {
     isEverydayChecked: function () {
@@ -73,6 +109,13 @@ export default {
     },
     isEveryMondayChecked: function () {
       return this.howmanynotify === "everymonday";
+    },
+    isValidEmail: function () {
+      return (
+        this.email != "" &&
+        this.email.length <= maxSize &&
+        pattern.test(this.email)
+      );
     },
   },
 };
