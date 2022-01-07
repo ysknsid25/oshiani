e6a57ac4468d498f1688c867cce0d019821104d3
ysknsid25 <ng_knok@docomo.ne.jp>
2022-01-07 13:18:37 +0900

    [update] GA文庫からデータ取得まで
    Signed-off-by: ysknsid25 <ng_knok@docomo.ne.jp>

--- functions/js/getagprogram/scraiping.js
+++ functions/js/getagprogram/scraiping.js
@@ -2,13 +2,12 @@ const fetch = require("node-fetch");
 const { JSDOM } = require("jsdom");
 
 exports.test = async (functions) => {
-    const res = await fetch("https://www.jma.go.jp/jp/week/319.html");
+    const res = await fetch("https://ga.sbcr.jp/release/month_current/");
     const html = await res.text();
     const dom = new JSDOM(html);
     const document = dom.window.document;
-    const nodes = document.querySelectorAll(
-        "#infotablefont tr:nth-child(4) td"
-    );
-    const tokyoWeathers = Array.from(nodes, (td) => td.textContent.trim());
-    functions.logger.info(tokyoWeathers, { structuredData: true });
+    const nodes = document.querySelectorAll(".newBook_item_Wrap");
+    const htmlTree = Array.from(nodes, (html) => html.textContent.trim());
+    //const tokyoWeathers = Array.from(nodes, (td) => td.textContent.trim());
+    functions.logger.info(htmlTree, { structuredData: true });
 };
